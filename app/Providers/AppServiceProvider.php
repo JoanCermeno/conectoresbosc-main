9182ea813bb89ad6ac627be33c2197077cc8a719
joan <joangabriel684@gmail.com>
2025-10-18 00:40:44 -0400

    quitando la validacion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,12 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //// Verifica si la aplicación está en un entorno de producción (o similar)
-        // y si la variable de entorno APP_ENV es 'production'.
-        if (config('app.env') === 'production' || env('APP_ENV') === 'production') {
-            // Esto le dice a Laravel que genere todos los URLs (incluyendo los de assets)
-            // usando el esquema HTTPS.
-            URL::forceScheme('https');
-        }
+     
     }
 }
